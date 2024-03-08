438f3833bc07095972af051db5d77db6bd22af58
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-03-08 08:55:26 +0800

    Added media card

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,7 +17,15 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Aladdinskie24</IonCardTitle>
+        <IonCardSubtitle>Student</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
       </IonContent>
     </IonPage>
   );
