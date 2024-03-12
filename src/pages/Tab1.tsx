e30d26c842e9ae01dd63dbfaf6af9f8be5280374
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-03-12 08:32:47 +0800

    Added button and alert function

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle  } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton,  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -25,7 +26,21 @@ const Tab1: React.FC = () => {
       </IonCardHeader>
 
       <IonCardContent>. Nothing more, nothing less.</IonCardContent>
+
+      <IonButton id="present-alert" expand="block">Quick Facts</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>
+
+  
     </IonCard>
+
+     
+
       </IonContent>
     </IonPage>
   );
