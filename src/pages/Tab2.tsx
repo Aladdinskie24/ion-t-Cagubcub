c5cab2daa855d656b324ca46f93ca6b6a93f8a8f
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-04-05 07:45:36 +0800

    Added click me to clickcounter

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,35 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React, { useState } from 'react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+    // State to keep track of the number of clicks
+    const [clickCount, setClickCount] = useState(0);
+
+    // Function to handle button click
+    const handleButtonClick = () => {
+        // Increment the click count
+        setClickCount(clickCount + 1);
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 2</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 2</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonButton onClick={handleButtonClick}>Click Me</IonButton>
+                {/* Click counter */}
+                <p>Number of Clicks: {clickCount}</p>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
