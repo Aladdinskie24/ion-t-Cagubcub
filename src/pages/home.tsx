871a5b73a7131c7c57d8da6d41ab85ef01fbd4f7
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-04-30 08:47:26 +0800

    added search bar on home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonIcon } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonButton,
+  IonIcon,
+  IonSearchbar
+} from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
-import ClickCounter from './Tab2';
 import { alarmOutline, calculatorOutline } from 'ionicons/icons';
 
+
+
 const Home: React.FC<RouteComponentProps> = ({ history }) => {
+  const [searchText, setSearchText] = useState('');
+
   const goToCalculator = () => {
     history.push('/calculator');
-  }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +35,12 @@ const Home: React.FC<RouteComponentProps> = ({ history }) => {
       </IonHeader>
 
       <IonContent fullscreen>
+        <IonSearchbar
+          value={searchText}
+          onIonChange={e => setSearchText(e.detail.value!)}
+          placeholder="Search here..."
+        />
+
         <IonCard>
           <img src="https://m.media-amazon.com/images/M/MV5BMzlkYTI3MWItMjU2NS00MjIzLTg2MzAtZDNiODQzMjBhMmM3XkEyXkFqcGdeQTNwaW5nZXN0._V1_QL75_UX500_CR0,0,500,281_.jpg" alt="Profile" />
           <IonCardHeader>
@@ -26,32 +50,29 @@ const Home: React.FC<RouteComponentProps> = ({ history }) => {
           <IonCardContent>Feel free to contact me when you have concerns. Love yahhhh:)</IonCardContent>
         </IonCard>
 
-<IonCard>
-    <IonCardContent>
-    <IonButton expand="block" routerLink='/Tab2'>
-    <IonIcon icon={alarmOutline} slot="start" />
-     Clickcounter
-    </IonButton>
-   </IonCardContent>
-
-
-   <IonCardContent>
-    <IonButton expand="block" routerLink='/Tab3'>
-    <IonIcon icon={calculatorOutline} slot="start" />
-     calculator
-    </IonButton>
-   </IonCardContent>
-
-   <IonCardContent>
-    <IonButton expand="block" routerLink='/tab4'>
-    <IonIcon icon={calculatorOutline} slot="start" />
-     todolist
-    </IonButton>
-   </IonCardContent>
-
-  </IonCard>
+        <IonCard>
+          <IonCardContent>
+            <IonButton expand="block" routerLink="/Tab2">
+              <IonIcon icon={alarmOutline} slot="start" />
+              Clickcounter
+            </IonButton>
+          </IonCardContent>
+          <IonCardContent>
+            <IonButton expand="block" routerLink="/Tab3">
+              <IonIcon icon={calculatorOutline} slot="start" />
+              Calculator
+            </IonButton>
+          </IonCardContent>
+          <IonCardContent>
+            <IonButton expand="block" routerLink="/tab4">
+              <IonIcon icon={calculatorOutline} slot="start" />
+              Todolist
+            </IonButton>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
 };
+
 export default Home;
