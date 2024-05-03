d1b2ae91993457c1c2ca8625cfa9a26e6199a07f
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-05-03 09:15:01 +0800

    added notes connection firebase

--- src/pages/firebase.tsx
+++ src/pages/firebase.tsx
@@ -1,42 +1,23 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-
-import { getFirestore } from 'firebase/firestore';
-
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDVrOvSu6Q28OOfinI1nR3PbCeeFy2xk5Y",
-
-  authDomain: "certain-density-400310.firebaseapp.com",
-
-  projectId: "certain-density-400310",
-
-  storageBucket: "certain-density-400310.appspot.com",
-
-  messagingSenderId: "179403612734",
-
-  appId: "1:179403612734:web:b0e979b44c0792864ca76f",
-
-  measurementId: "G-3XE6Z3V18X"
-
+  apiKey: "AIzaSyATtieoLHQ2qn8fRVlYQ9diDPktzJWISUM",
+  authDomain: "it35-cagubcub-81800.firebaseapp.com",
+  projectId: "it35-cagubcub-81800",
+  storageBucket: "it35-cagubcub-81800.appspot.com",
+  messagingSenderId: "1054627829175",
+  appId: "1:1054627829175:web:efd9305a699c3b95ee8432",
+  measurementId: "G-67XJWNW251"
 };
-
-
-// Initialize Firebase
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-// 
 const db = getFirestore(firebaseApp);
 
 export{db}
