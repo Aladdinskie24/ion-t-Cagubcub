b53465b82d96dc58f7dfaaf7d85d71bde14de9b8
Aladdinskie24 <20201069@nbsc.edu.ph>
2024-05-10 08:48:53 +0800

    Updates

--- assets/ios.transition-legacy-Csk4WQRN.js
+++ assets/ios.transition-legacy-Csk4WQRN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wx6GzQOw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BsGOuful.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Db1EnFgs.js
+++ assets/swipe-back-legacy-Db1EnFgs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wx6GzQOw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BsGOuful.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
